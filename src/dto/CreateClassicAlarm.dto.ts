4b365c957fb9bb2b862bb04df926a9bb435a21d7
f-pochat <federico.pochat@ing.austral.edu.ar>
2022-11-01 17:15:19 -0300

    Workflow

--- src/dto/CreateClassicAlarm.dto.ts
+++ src/dto/CreateClassicAlarm.dto.ts
@@ -4,19 +4,24 @@ import {
   IsEmpty,
   IsNotEmpty,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateClassicAlarmDto {
   @IsNotEmpty()
   @IsDateString()
+  @ApiProperty()
   time: Date;
 
   @IsNotEmpty()
   @IsByteLength(1, 50)
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   days: number[];
 
   @IsNotEmpty()
+  @ApiProperty()
   deviceId: string;
 }
--- src/dto/CreateSmartAlarm.dto.ts
+++ src/dto/CreateSmartAlarm.dto.ts
@@ -4,27 +4,35 @@ import {
   IsEmpty,
   IsNotEmpty,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateSmartAlarmDto {
   @IsNotEmpty()
   @IsByteLength(1, 50)
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   alarmLocationLat: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   alarmLocationLong: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   destinationLocationLat: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   destinationLocationLong: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   preparationTime: number;
 
   @IsNotEmpty()
+  @ApiProperty()
   deviceId: string;
 }
